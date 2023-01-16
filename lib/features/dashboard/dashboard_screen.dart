d0e6f571eafd4b21c38967c09dbca8517111a199
Christian Alexsander Santos <christianalexbh@hotmail.com>
2023-01-16 11:22:18 -0300

    feat: bottom app bar

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -1,5 +1,3 @@
-import 'package:card_nft_app/constants.dart';
-import 'package:card_nft_app/features/auth/application/auth_application.dart';
 import 'package:card_nft_app/features/dashboard/widgets/deck_data_cards.dart';
 import 'package:card_nft_app/theme/theme.dart';
 import 'package:flutter/material.dart';
@@ -15,17 +13,32 @@ class DashboardScreen extends StatelessWidget {
         backgroundColor: AppTheme.darkCharcoal,
         automaticallyImplyLeading: false,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          authConnetion.logout();
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () async {
+      //     authConnetion.logout();
 
-          Navigator.pushNamed(context, RouterPaths.login);
-        },
-        backgroundColor: Colors.pink,
-        child: const Icon(Icons.logout_outlined),
-      ),
+      //     Navigator.pushNamed(context, RouterPaths.login);
+      //   },
+      //   backgroundColor: Colors.pink,
+      //   child: const Icon(Icons.logout_outlined),
+      // ),
       backgroundColor: AppTheme.arsenic,
       body: const DeckDataCards(),
+
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: AppTheme.arsenic,
+        elevation: 20.0,
+        child: Container(height: 50.0),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment Counter',
+        backgroundColor: AppTheme.blurple,
+        onPressed: () {},
+        elevation: 5,
+        child: const Icon(Icons.casino_sharp),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
